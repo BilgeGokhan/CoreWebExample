c11f65538d55eb96bbb71f19bd1f300bbf2c34db
BilgeGokhan <47634641+BilgeGokhan@users.noreply.github.com>
2020-05-26 00:45:34 +0300

    Initial commit

---
+++ CoreWebExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreWebExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
